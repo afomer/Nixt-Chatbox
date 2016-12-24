59ed256ec212ae048f0c2e65910cc9d9c95208f2
Abubaker <AbubakerFaisal@Abubakers-MacBook-Pro.local>
2016-12-24 19:13:12 +0300

    back

--- client.c
+++ client.c
@@ -420,17 +420,18 @@ void ReadingChatFromServer(void *serverfd_ptr)
 {
     int serverfd = *(int *)(long *)(serverfd_ptr);
     char server_buf[MAXLINE];
-    rio_t serverfd_rio;
-    rio_readinitb( serverfd_rio, serverfd); 
     int server_buf_not_empty;
     // char * friend_name ;
     while (true)
     { 
+        /// Recieving Other participants Chat, if there is
+        ioctl(serverfd, SIOCINQ, &server_buf_not_empty);
+        
+        if ( server_buf_not_empty )
+        {   
 
-        rio_readlineb(serverfd, server_buf, MAXLINE);
-        printf("Server sends me: %s", server_buf);
             read(serverfd, server_buf, MAXLINE);
-            printf("Server sends me: %s", server_buf);
+            printf("Server sends me: %s\n", server_buf);
 
 
             // printf(">> ");
@@ -446,7 +447,7 @@ void ReadingChatFromServer(void *serverfd_ptr)
             // {
             //     // printf("recieved and not showing (aka success?!?)\n");
             // }
-
+        }
     }
 
     return;
